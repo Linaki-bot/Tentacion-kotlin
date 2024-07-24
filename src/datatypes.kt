a86e1473c7a924bb5317de7f3f9d602d6fbddbd2
shanelle-create <janettemoh@gmail.com>
2024-07-24 13:19:46 +0300

    initial commit

---
+++ src/datatypes.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var number:Int = 90 //Int
+    var num:Double = 78.01 //Double
+    var mydecimal:Float = 67.12F //Float
+    var greeting:String = "Hello there" //String
+    var consonant:Char = 'B' //Character
+    var isKotlinInteresting:Boolean = true //Boolean
+
+    println(number)
+    println(num)
+    println(mydecimal)
+    println(greeting)
+    println(consonant)
+    println(isKotlinInteresting)
+}
---
+++ src/introduction.kt
@@ -0,0 +1,12 @@
+fun main () {
+
+    println("Hello world")
+    println("Lizminah")
+    print(100)
+    print("Kotlin")
+    println(20+15)
+
+
+
+
+}
